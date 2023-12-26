98fa411be0b3ba232cd85383d0c2d14439789b1e
Lydia <frankenshteynle@gmail.com>
2023-12-26 19:09:51 +0300

    добавили первый вариант решения задачи

---
+++ program/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        string[] originalArray = { "horse", "cat", "dog", "mice", "ant" };
+        string[] newArray = new string[0];
+        foreach (string str in originalArray)
+        {
+            if (str.Length <= 3)
+            {
+                Array.Resize(ref newArray, newArray.Length + 1);
+                newArray[newArray.Length - 1] = str;
+            }
+        }
+        Console.Write("Новый массив строк из символов длиной <= 3: ");
+        foreach (string str in newArray)
+        {
+            Console.Write($"{str}, ");
+        }
+    }
+}
