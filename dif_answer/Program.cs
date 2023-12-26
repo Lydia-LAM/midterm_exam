f8dbb7127d19ae3737fb8721e7df9f1cc91b7c4c
Lydia <frankenshteynle@gmail.com>
2023-12-26 19:13:13 +0300

    добавили другой вариант решения задачи через ввод с клавиатуры

---
+++ dif_answer/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Введите строки через запятую:");
+        string input = Console.ReadLine();
+        string[] originalArray = input.Split(new char[] { ',' }, StringSplitOptions.RemoveEmptyEntries);
+        string[] newArray = FilterStrings(originalArray);
+        Console.WriteLine("Новый массив строк длиной <= 3 символам:");
+        foreach (string str in newArray)
+        {
+            Console.WriteLine(str);
+        }
+
+        Console.ReadLine();
+    }
+    static string[] FilterStrings(string[] originalArray)
+    {
+        int count = 0;
+        foreach (string str in originalArray)
+        {
+            if (str.Length <= 3)
+            {
+                count++;
+            }
+        }
+        string[] newArray = new string[count];
+        int index = 0;
+        foreach (string str in originalArray)
+        {
+            if (str.Length <= 3)
+            {
+                newArray[index] = str;
+                index++;
+            }
+        }
+        return newArray;
+    }
+}
